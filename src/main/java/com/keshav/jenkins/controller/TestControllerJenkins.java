1e4ccc4f4c098e89880e6ece9eb99d5a0590b294
Keshav <aggarwalk721@gmail.com>
2021-11-15 15:59:22 +0530

    first commit

--- src/main/java/com/keshav/jenkins/controller/TestControllerJenkins.java
+++ src/main/java/com/keshav/jenkins/controller/TestControllerJenkins.java
@@ -1,9 +1,12 @@
 package com.keshav.jenkins.controller;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.keshav.jenkins.dto.Student;
@@ -12,11 +15,13 @@ import com.keshav.jenkins.dto.Student;
 public class TestControllerJenkins {
 
 	@GetMapping("/Welcome/{name}")
-	public String printName(@PathVariable("name") String name)
+	public String printName(@PathVariable("name") String name, @RequestParam("Age") String age)
 	{
-		return "Hello! Welcome "+name;
+		
+		return "Hello! Welcome "+name+" and your age is: "+age;
 		
 	}
+	
 	
 	@PostMapping("/addStudent")
 	public Student addStudent(@RequestBody Student student)
